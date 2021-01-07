32c48e410b35e983e37cd749c6bc408f603d1235
Mustafa Sharaf <72881858+mustafasharaf04@users.noreply.github.com>
2021-01-06 19:36:06 -0800

    Update main.py

--- Mario Musuem/main.py
+++ Mario Musuem/main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def home():
     return "Welcome to the Mario Museum!"
     <iframe src="https://drive.google.com/file/d/1YzrzgJ5fCn_RSHAH9E8uOjajw0BCb1uS/preview" width="640" height="480"></iframe>
-
-if __name__ == "__main__":
+    <iframe src="https://drive.google.com/file/d/11V-Sfh0OpmEyMew5W2G0s0y-kj37HSjc/preview" width="640" height="480"></iframe>
+    <iframe src="https://drive.google.com/file/d/1VFg-s_0Urk_WyT1b23Fy6bn5ynNOhxwP/preview" width="640" height="480"></iframe>   
+    <iframe src="https://drive.google.com/file/d/16gkPdeOCOjd6wan4e0CNdDJoH7YDE6NB/preview" width="640" height="480"></iframe>    if __name__ == "__main__":
     app.run()
