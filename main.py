e42cc3cc79ca0533dbd0c2ed9ca46f4f653f89b8
nguyenjacob <72947143+nguyenjacob@users.noreply.github.com>
2021-01-20 12:38:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,25 @@ def home():
     return render_template('home.html')
 
 
+@app.route("/sm64")
+def sm64():
+    return render_template("sm64.html")
+
+
+@app.route("/mariosunshine")
+def mariosunshine():
+    return render_template("mariosunshine.html")
+
+
+@app.route("/galaxy")
+def galaxy():
+    return render_template("galaxy.html")
+
+
+@app.route("/odyssey")
+def odyssey():
+    return render_template("odyssey.html")
+
+
 if __name__ == "__main__":
     app.run()
