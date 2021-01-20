82976cb8bb307e8f004ddc298567581f5bb4c39c
nguyenjacob <72947143+nguyenjacob@users.noreply.github.com>
2021-01-20 12:40:31 -0800

    Update main.py

--- Mario Musuem/main.py
+++ Mario Musuem/main.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Welcome to the Mario Museum!"
-   if __name__ == "__main__":
+    return render_template('home.html')
+
+
+@app.route("/sm64")
+def sm64():
+    return render_template("sm64.html")
+
+
+@app.route("/mariosunshine")
+def mariosunshine():
+    return render_template("mariosunshine.html")
+
+
+@app.route("/galaxy")
+def galaxy():
+    return render_template("galaxy.html")
+
+
+@app.route("/odyssey")
+def odyssey():
+    return render_template("odyssey.html")
+
+
+if __name__ == "__main__":
     app.run()
