9543e277c844dd0333bab8ec91e86f4296d4ce73
x-Dk7 <73075476+x-Dk7@users.noreply.github.com>
2021-02-08 13:10:06 -0800

    updated mini-game.html working on main.py

--- mini-game/main.py
+++ mini-game/main.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/mini-game")
+def mini-game() :
+    return render_template('mini-game.html')
+
+if __name__ == "__main__":
+    app.run()
