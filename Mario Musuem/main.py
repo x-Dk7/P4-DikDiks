bf83dd4d3b85f14ba319f4163da9c3dba5fdeafc
x-Dk7 <73075476+x-Dk7@users.noreply.github.com>
2021-02-11 13:03:32 -0800

    updated mini-game.html working on main.py

--- Mario Musuem/main.py
+++ Mario Musuem/main.py
@@ -2,36 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/egg")
-def egg() :
-    return render_template('egg.html')
-
+@app.route("/game")
+def game():
+    return render_template('mini-game.html')
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
-
 @app.route("/sm64")
 def sm64():
     return render_template("sm64.html")
 
-
-@app.route("/mariosunshine")
-def mariosunshine():
+@app.route("/sunshine")
+def sunshine():
     return render_template("mariosunshine.html")
-
 
 @app.route("/galaxy")
 def galaxy():
     return render_template("galaxy.html")
 
-
 @app.route("/odyssey")
 def odyssey():
     return render_template("odyssey.html")
 
+@app.route("/egg")
+def egg() :
+    return render_template('egg.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
